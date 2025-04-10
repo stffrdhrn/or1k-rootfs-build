112c3ee4f102f5e49ff743f06a66f6c0c32d6c63
Stafford Horne <shorne@gmail.com>
2025-04-10 17:32:08 +0100

    build: Add missing files

---
+++ or1k-rootfs-build/package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+version=`date -u +%Y%m%d`
+
+package_dir()
+{
+  declare dir=$1 ; shift
+
+  # Create archive/
+  pkg_root=${dir}-${version}
+  mv $dir $pkg_root
+  tar -Jcf /opt/rootfs/output/$pkg_root.tar.xz $pkg_root &
+}
+
+[ $BUSYBOX_ENABLED ] && package_dir "busybox-rootfs"
+[ $BUILDROOT_ENABLED ] && package_dir "buildroot-rootfs"
+wait
+
